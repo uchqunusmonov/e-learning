4d19a7d66219ec2e3861e32bebdf17da94886be2
uchqunusmonov <usmonov_uchqun@mail.ru>
2023-03-13 09:16:26 +0500

    test

--- config/settings/base.py
+++ config/settings/base.py
@@ -19,7 +19,7 @@ from django.urls import reverse_lazy
 load_dotenv('.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -139,9 +139,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, '../../static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, '../../staticfiles')
 # Media files
 
 MEDIA_URL = 'media/'
